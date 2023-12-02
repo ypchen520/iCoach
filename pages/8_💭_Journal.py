80e318ceb4174858a4a3caf53cee1748a8b5f3b1
Yu-Peng Chen <yupeng@Yu-Pengs-MacBook-Air.local>
2023-12-02 15:43:21 -0500

    trying new design for Chat

--- pages/8_💭_Journal.py
+++ pages/8_💭_Journal.py
@@ -25,3 +25,15 @@ import streamlit as st
 # """
 
 st.title(':thought_balloon: Journal')
+
+# Quote
+# There is a fountain of youth: it is your mind, your talents, the creativity you bring to your life and the lives of people you love. When you learn to tap this source, you will truly have defeated age
+# Just before the sparks of life are extinguished from a candle, the flame dances. It sends a wistful, thin smoke line up into the air, where it circles and pirouettes before it vanishes toward the sky. Light a candle and watch that dance, learn about life and its last breaths.
+# Keep your flame lit, let it dance in your heart, and you won’t disappear. The world will still see you. Because you’re lighting the way for all who follow.
+
+
+# Python
+# A nice and clean generator expression here, an elegant use of the "with"-statement there…
+# With some practice you can do this tastefully—only where these features make sense and help make the code more expressive.
+# And trust me, your colleagues will pick up on this after a while. If they ask you questions, be generous and helpful.
+# Pull everyone around you UP and help them learn what you know.
