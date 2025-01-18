a1dc3383329a24641757ea1295ab48656bba17f8
Yu-Peng <yupeng>
2025-01-18 13:48:11 -0500

    🎧

--- pages/4_🎧_Progress.py
+++ pages/4_🎧_Progress.py
@@ -4,23 +4,25 @@ from st_circular_progress import CircularProgress
 # tabs: daily, weekly, monthly
 daily_tab, weekly_tab, monthly_tab = st.tabs([":sunny: Daily", ":calendar: Weekly", ":full_moon: Monthly"])
 
-daily_minute_goal = 330
-daily_accumulated_minutes = 17 # pull data from fb
+# daily_minute_goal = 330
+# daily_accumulated_minutes = 17 # pull data from fb
 
 with daily_tab:
+    daily_minute_goal = st.number_input(":trophy: Daily minute goal: ", value=300)
+    daily_accumulated_minutes = st.number_input(":mechanical_arm: Daily accumulated minutes: ", value=0)
     # st.markdown("<style>h2{}</style>", unsafe_allow_html=True)
     st.markdown("<style>div.block-container{text-align: center;}</style>", unsafe_allow_html=True)
-    st.write(f":trophy: Daily minute goal: {daily_minute_goal}")
-    st.write(f":mechanical_arm: Daily accumulated minutes: {daily_accumulated_minutes}")
+    # st.write(f":trophy: Daily minute goal: {daily_minute_goal}")
+    # st.write(f":mechanical_arm: Daily accumulated minutes: {daily_accumulated_minutes}")
 
     st.write("###")
 
     progress_percentage = int(daily_accumulated_minutes / daily_minute_goal * 100)
 
     if progress_percentage < 100:
-        text = f"Keep it up, you can do this! {progress_percentage}%"
+        text = f":fist: Keep it up, you can do this! {progress_percentage}%"
     else:
-        text = "You're done for today!"
+        text = ":star: Another day, another win!"
         progress_percentage = 100
 
     st.progress(progress_percentage, text)
